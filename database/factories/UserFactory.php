a283db7d39f9c4bfbccf0cc90917875d25a5e45e
curder <q.curder@gmail.com>
2021-12-15 16:54:54 +0800

    Change user avatar default url

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,7 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'username' => $this->faker->userName(),
-            'avatar' => 'https://pravatar.cc/images/164?img=' . $this->faker->biasedNumberBetween(1, 70),
+            'avatar' => 'https://i.pravatar.cc/164?img=' . $this->faker->biasedNumberBetween(1, 70),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
